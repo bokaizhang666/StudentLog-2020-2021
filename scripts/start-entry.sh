58c4414c3b667599a9065d1378d0ce29510f5bb3
Bokai Zhang <bzhang2023@chadwickschool.org>
2020-09-26 16:21:17 -0700

    Fix errors for log script again

--- scripts/start-entry.sh
+++ scripts/start-entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-FILENAME="$(echo "logs/$(date +'%Y-%m-%d').log.md")
+FILENAME=$(echo "logs/$(date +'%Y-%m-%d').log.md")
 
 touch $FILENAME
 echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
